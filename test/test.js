09e63aac546ab9cb6d49440e9ef1e2a68509d05e
MisterMagikarp <jonathan.baron@caramail.fr>
2014-10-23 15:09:24 +0200

    first commit

--- test/test.js
+++ test/test.js
@@ -3,4 +3,9 @@
 var PalettoTestCase = TestCase("PalettoTestCase");
 
 PalettoTestCase.prototype.testStory1 = function () {
+    var Engine = new Engine();
+    assertEquals(Engine.check_length_board(),36);
+    assertNotEquals(Engine.initialisation_board(),null);
+
+
 };
